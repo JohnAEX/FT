69844eff086c00ae6f22e0c4c894383bd5a38d4a
JohnAEX <grenda.jonathan@gmail.com>
2018-04-04 15:45:52 +0200

    Init Mvn Project
    Pushing MvnProject

---
+++ flattask/src/main/java/hwr/sem4/csa/App.java
@@ -0,0 +1,13 @@
+package hwr.sem4.csa;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
